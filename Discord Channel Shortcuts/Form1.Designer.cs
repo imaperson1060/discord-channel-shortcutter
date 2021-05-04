7209b145f807e581bc012f49631d21019f0505dd
arimeisels3@gmail.com <me@arimeisels.com>
2021-05-03 22:33:33 -0400

    source

---
+++ Discord Channel Shortcuts/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Discord_Channel_Shortcuts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.start = new System.Windows.Forms.Button();
+            this.findIDs = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(24, 22);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(129, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // findIDs
+            // 
+            this.findIDs.Location = new System.Drawing.Point(185, 22);
+            this.findIDs.Name = "findIDs";
+            this.findIDs.Size = new System.Drawing.Size(129, 23);
+            this.findIDs.TabIndex = 1;
+            this.findIDs.Text = "How to Find IDs";
+            this.findIDs.UseVisualStyleBackColor = true;
+            this.findIDs.Click += new System.EventHandler(this.findIDs_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 66);
+            this.Controls.Add(this.findIDs);
+            this.Controls.Add(this.start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Discord Shortcut Maker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button findIDs;
+    }
+}
+
---
+++ Discord Channel Shortcuts/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using IWshRuntimeLibrary;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace Discord_Channel_Shortcuts
+{
+    public partial class Form1 : Form
+    {
+        private void MakeShortcut(long server, long channel, string channelName)
+        {
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            using (StreamWriter writer = new StreamWriter(desktop + "\\" + channelName + ".url"))
+            {
+                writer.WriteLine("[InternetShortcut]");
+                writer.WriteLine("URL=discord://discordapp.com/channels/" + server + "/" + channel);
+                writer.Flush();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            MakeShortcut(Convert.ToInt64(Interaction.InputBox("Server ID:", "Shortcut Maker (1/3)", "", 100, 100)), Convert.ToInt64(Interaction.InputBox("Channel ID:", "Shortcut Maker (2/3)", "", 100, 100)), Convert.ToString(Interaction.InputBox("Shortcut Name:", "Shortcut Maker (3/3)", "", 100, 100)));
+            Close();
+        }
+
+        private void findIDs_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID");
+        }
+    }
+}
